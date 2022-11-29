7bedd202a7f08a29a7f2ec169d36e58c7ac06cb3
lucaspontoexe <ltx16@hotmail.com>
2022-11-28 23:29:29 -0300

    e tem css na logo

--- src/lib/Logo.svelte
+++ src/lib/Logo.svelte
@@ -6,3 +6,19 @@
 	<h1>DESCONFIE</h1>
 	<img src={image} alt="logo desconfie" />
 </div>
+
+
+<style>
+.logo {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    height: 8rem;
+    gap: 2em;
+}
+
+h1 {
+    font-family: 'Inter','Roboto', sans-serif;
+    font-size: 4rem;
+}
+</style>
