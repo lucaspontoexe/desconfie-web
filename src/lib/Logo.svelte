4564f655d2b67d7869a45bc77778de4ca71151e8
lucaspontoexe <ltx16@hotmail.com>
2022-11-28 23:23:00 -0300

    logo tá na mão, a formatação de datas também

--- src/lib/Logo.svelte
+++ src/lib/Logo.svelte
@@ -1,1 +1,8 @@
-<div class="logo">eis a logo</div>
+<script>
+	import image from '$lib/assets/desconfie_logo.png';
+</script>
+
+<div class="logo">
+	<h1>DESCONFIE</h1>
+	<img src={image} alt="logo desconfie" />
+</div>
---
+++ src/lib/formatCreationDate.ts
@@ -0,0 +1,7 @@
+
+export function creationDate(date:Date) {
+    const formattedDate = new Intl.DateTimeFormat('pt-BR').format(date);
+    const dateDifference = (new Date().getTime() - date.getTime()) / 86400_000;
+
+    return `Criado em ${formattedDate} (${dateDifference}) dias`
+}
