03ee89b7963ac8fa89d89362cd75095d3b22a2a7
lucaspontoexe <ltx16@hotmail.com>
2022-11-28 23:06:59 -0300

    o esqueleto tá saindo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,34 @@
-<script lang="ts">
-    import Logo from '$lib/Logo.svelte';
+<script>
+	import Logo from '$lib/Logo.svelte';
 </script>
 
 <main>
-    <Logo/>
-    <h1>Welcome to SvelteKit</h1>
-    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<Logo />
+
+	<form action="result" method="post">
+		<input type="url" name="site" id="site" />
+		<button type="submit">Verificar</button>
+	</form>
 </main>
 
+<section>
+	<h2>Seguro 🟢</h2>
+	<div class="subitile">Pontos fortes do site buscado:</div>
+	<ul>
+		<li>Site ativo</li>
+		<li>Possui HTTPS</li>
+		<li>Criado em 06-10-1997 (9183 dias)</li>
+	</ul>
+</section>
+
+Suspeito 🟡 <br>
+Pontos suspeitos do site buscado: 
+<br>
+
+Não Seguro 🔴 <br>
+Pontos não seguros do site buscado:
+
+
+<div class="todo">
+    ideia: mostrar quando o site 
+</div>
